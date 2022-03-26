c2cf6ea6e9427d235ec3b055fb6983306ede6a29
Tello- <tellovisionstudio@gmail.com>
2022-03-25 17:42:29 -0700

    Init Commit

---
+++ MCL_LIB/mcl.h
@@ -0,0 +1,35 @@
+#pragma once
+
+namespace MCL
+{
+	namespace Types
+	{
+		// Some Possible Data Structures to Implement 
+		/*
+		class Array;
+		class List;
+		class DynamicArray;				// Array with dynamic memory allocation, no more features than your standard array
+		class Vector;					// Dynamic Array with more features, type safety and manipulation options
+		class ArrayStyle_LinkedList;		// A Linked List built out of an array. (multiple ways to do this)
+		class NodeStyle_LinkedList;		// Node and Pointer style Linked List.
+		class Array_Stack;				// Stack interface // Array back-end
+		class Vector_Stack;				// Stack interface // Vector back-end
+		class LinkedNode_Stack;			// Stack interface // Linked-List back end
+		class Array_Queue;				// Stack interface // Array back-end
+		class Vector_Queue;				// Stack interface // Vector back-end
+		class PriorityQueue;
+		class LinkedNode_Queue;			// Stack interface // Linked-List back end
+		class Bag;					// Walls & Mirrors : Chapter 1.5
+		class Tree;					// Generic Tree : no binary restriction
+		class BinaryTree;				// binary tree
+		class BinarySearchTree;			// Binary Search Tree
+		class Graph;
+		class DirectedGraph;
+		class DirectedAcyclicGraph;		// DAG
+		*/
+	}
+	namespace Algorithms
+	{
+
+	}
+}
